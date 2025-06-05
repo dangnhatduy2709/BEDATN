99099931677ea4ac62d06b0d11f135954aa88f58
nhatviet <daonhatviet.dev@gmail.com>
2025-06-05 23:04:46 +0700

    add comment

--- routers/Comment.js
+++ routers/Comment.js
@@ -3,13 +3,10 @@ var db = require('./DBConnect');
 
 // tạo chức năng đăng nhập
 router.post('/add', function (req, res) {
-    var taskid = req.body.taskID;
-    var userid = req.body.userID;
-    var commenttext = req.body.commentText;
-    var attachmentdata = req.body.attachmentData;
-    var createddate = req.body.createdDate;
-    var query = "INSERT INTO Comments (taskID, userID, commentText, attachmentData, createdDate, isDeleted) VALUES (?, ?, ?, ?, ?, 0)";
-    db.query(query, [taskid, userid, commenttext, attachmentdata, createddate], function (err, result) {
+    const { userID, taskID, message } = req.body;
+
+    var query = "INSERT INTO comments (task_id, user_id, message, attachment_path, is_read) VALUES (?, ?, ?, ?, ?)";
+    db.query(query, [taskID, userID, message, null, 0], function (err, result) {
         if (err) {
             console.error('Error executing query:', err);
             res.status(500).json({ error: 'Error executing query', details: err.message });
@@ -20,8 +17,26 @@ router.post('/add', function (req, res) {
 });
 
 // Lấy danh sách đăng nhập
+router.get('/getCommentByTaskID/:id', function (req, res) {
+  var query = `
+    SELECT c.*, t.*, u.* 
+    FROM comments c
+    LEFT JOIN tasks t ON c.task_id = t.taskID
+    LEFT JOIN users u ON c.user_id = u.userID
+    WHERE c.task_id = ?
+    ORDER BY c.sent_at DESC`;
+  
+  db.query(query, [req.params.id], function (err, result) {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Database query error' });
+    }
+    res.status(200).json(result);
+  });
+});
+
 router.get('/', function (req, res) {
-    var query = "select * from Comments";
+    var query = "select * from Comments ";
     db.query(query, function (err, result) {
         if (err) throw err;
         res.status(200).json(result);
